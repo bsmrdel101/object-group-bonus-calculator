ba28c9d456255a1dab23ee1554bc6a56534e559a
Bennett Smrdel <smrdelb@gmail.com>
2021-11-02 16:29:28 -0500

    finished

--- client.js
+++ client.js
@@ -40,3 +40,77 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+//create function that takes employees as an argument
+  //return new object with name, bonusPercentage, totalCompensation, totalBonus
+
+  function newEmployee(employee) {
+    let allEmployees = [];
+    let employeeBonus = totalBonus(employee);
+      
+    let employeesList = {
+      name: employee.name,
+      totalBonus: employeeBonus,
+      totalCompensation: Number(employeeBonus) + Number(employee.annualSalary),
+      bonusPercentage: percentage(employee, employeeBonus)
+    }
+    allEmployees.push(employeesList);
+    return allEmployees;
+  }
+
+//loop through employees, check review rating
+//assign percentages using a conditional
+function bonusCalcOne(employee) {
+  if (employee.reviewRating === 5) {
+    return employee.annualSalary * 0.1;
+  } else if (employee.reviewRating === 4) {
+    return employee.annualSalary * .06;
+  } else if (employee.reviewRating === 3) {
+    return employee.annualSalary * .04;
+  } else {
+    return 0;
+  }
+}
+
+function bonusCalcTwo(employee) {
+  //detect if number is 4 digits
+  if (employee.employeeNumber.length === 4) {
+    return employee.annualSalary * .05;
+  } else {
+    return 0;
+  }
+}
+
+function totalBonus(employee) {
+  let bonusOne = bonusCalcOne(employee);
+  let bonusTwo = bonusCalcTwo(employee);
+  let totalBonus = bonusOne + bonusTwo;
+
+  if (employee.annualSalary > 65000) {
+    totalBonus *= .99;
+  }
+  if (totalBonus > employee.annualSalary * .13) {
+    totalBonus = employee.annualSalary * .13;
+  } else if (totalBonus < 0){
+    totalBonus = 0;
+  }
+  return totalBonus;
+}
+
+function percentage(employee, totalBonus) {
+  percent = totalBonus / employee.annualSalary;
+  return percent * 100;
+}
+
+console.log('employee 1:', totalBonus(employees[0]));
+console.log('employee 2:', totalBonus(employees[1]));
+console.log('employee 3:', totalBonus(employees[2]));
+console.log('employee 4:', totalBonus(employees[3]));
+console.log('employee 5:', totalBonus(employees[4]));
+console.log('-----');
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(newEmployee(employees[i]));
+}
+
+//totalBonus / annuel salery
